8151c3370902554f865a495299acef2375a61f40
Joseph Sgrignoli <JSgrignoli@hotmail.com>
2022-06-09 21:19:16 -0400

    added in turtle draw screen and created function to drawn tictctoe grid

--- name.py
+++ name.py
@@ -1,8 +1,39 @@
+import turtle
+s = turtle.getscreen()
+t = turtle.Turtle()
+turtle.title("TicTacToe")
+t.speed(1)
+
 def draw_board():
-   print( """""
-___|___|___
-___|___|___
-   |   |   
-    """)
+    # first horizontal line
+    t.penup()
+    t.goto(-200, 75)
+    t.pendown()
+    t.goto(200, 75)
+
+    # second horizontal line
+    t.penup()
+    t.goto(-200, -75)
+    t.pendown()
+    t.goto(200, -75)
+
+    # first vertical line
+    t.penup()
+    t.goto(-75, -200)
+    t.pendown()
+    t.goto(-75, 200)
+
+    # second vertical line
+    t.penup()
+    t.goto(75, -200)
+    t.pendown()
+    t.goto(75, 200)
+
+
+
+
+
+
+    input("Waiting")
 
 draw_board()
